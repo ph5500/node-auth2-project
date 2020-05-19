e2fd8e9c803299571a48a813a6c61008614ec6a2
ph5500 <philfives@gmail.com>
2020-05-19 16:15:50 -0600

    added more files

---
+++ migrations/20200519144426__authorizer.js
@@ -0,0 +1,27 @@
+
+exports.up = function (knex) {
+    return knex.schema
+        .createTable('roles', tbl => {
+            tbl.increments();
+            tbl.string('name', 128).notNullable().unique();
+        })//rolls
+
+
+        .createTable('users', tbl => {
+            tbl.increments()
+            tbl.string('username', 128).notNullable().unique().index()
+            tbl.string('password', 256).notNullable();
+
+            tbl.integer('role')
+                .unsigned()
+                .references('roles.id')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE')
+        })//users
+
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists("roles").dropTableIfExists("users");
+
+};
